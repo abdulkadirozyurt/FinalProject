2d12b00a361554d187224b475290f1f312a6699b
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-03-17 18:34:44 +0300

    Add Messages class for product operation messages
    Introduced a new static class `Messages` in the `Business.Constants` namespace. This class contains two string constants: `ProductAdded` for successful product addition and `ProductNameInvalid` for validation of product names, enhancing user feedback during product operations.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Product added successfully";
+        public static string ProductNameInvalid = "Product name must be at least 2 characters long";
+    }
+}
