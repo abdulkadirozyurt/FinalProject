3c17ea89c30165a405a17d75c8eeb0646dcc54ff
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-03-17 17:25:21 +0300

    Add constructors to Result class for better initialization
    Updated the `Result` class in the `Core.Utilities.Results` namespace to include constructors that initialize the `Success` property. This change allows for more flexible object creation with two constructors: one for a `bool success` and another for both a `bool success` and a `string message`.

--- Core/Utilities/Results/Result.cs
+++ Core/Utilities/Results/Result.cs
@@ -8,8 +8,16 @@ namespace Core.Utilities.Results
 {
     public class Result : IResult
     {
-        public bool Success { get; }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+        public Result(bool success, string message) : this(success)
+        {
+            Message = message;
+        }
 
+        public bool Success { get; }
         public string Message { get; }
     }
 }
