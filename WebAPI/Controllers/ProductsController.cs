d7478bc27c4d8cefb5d3b824330b1aa2a3cd02c6
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2025-03-24 01:04:49 +0300

    Add WebAPI project with controllers and configuration
    This commit introduces a new "WebAPI" project to the solution, including:
    - `ProductsController` and `WeatherForecastController` for handling product operations and weather data retrieval.
    - `Program.cs` and `Startup.cs` to set up the web host and configure services, including Swagger for API documentation.
    - Project file `WebAPI.csproj` with necessary dependencies and target framework.
    - Configuration files `appsettings.json` and `appsettings.Development.json` for logging and application settings.
    - Updated solution file to include the new project and its build configurations.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [SwaggerTag("Products Operations")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        [SwaggerOperation("khbkhvkvkvk")]
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,77 @@
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            // create a reference for me
+            // AddSingleton: It creates a single instance of the object in whole memory and reuses it.
+            // whether there are a million requests or a single request, it will always use the same object.
+            // but we use singleton when we don't have data in it.
+            // if we use a cart state in a e-commerce platform, in a manager class, it causes mix each other cart information
+            services.AddSingleton<IProductService, ProductManager>();
+            services.AddSingleton<IProductDal,EfProductDal>();
+
+        
+
+
+
+            services.AddSwaggerGen(options =>
+            {
+                options.EnableAnnotations();
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
